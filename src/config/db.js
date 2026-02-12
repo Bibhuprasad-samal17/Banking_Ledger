337ab752ed706e4f67007e5243e95ffaf93f15ac
Bibhuprasad <bibhuprasadsamal1711@gmail.com>
2026-02-12 16:00:09 +0530

    server set up

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+
+
+function connectToDB() {
+
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process with an error code
+    });
+}
+
+module.exports = connectToDB;
